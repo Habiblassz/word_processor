752c0ec31d4b7e57a7da760edac477c55b2d6a06
Lawal Habib <lasszcodes@gmail.com>
2026-03-15 14:54:53 +0100

    add more punctuation to test

--- processors/format_isolated_punctuation.go
+++ processors/format_isolated_punctuation.go
@@ -5,7 +5,7 @@ func Format_Isolated_Punctuation(s []string) []string {
 	for idx, word := range s {
 
 		switch word {
-		case "...", ",", "?", ".", "!", ":", ";":
+		case "...", ",", "?", ".", "!", ":", ";", "!?":
 			if idx > 0 {
 				s[idx-1] = s[idx-1] + word
 				s[idx] = ""
